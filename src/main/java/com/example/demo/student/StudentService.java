0e1af245841b7ec27f9e8bd3eda72c4c779828f9
VakZok <100447067+VakZok@users.noreply.github.com>
2022-09-26 22:33:42 +0200

    Dependency injections

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,10 +1,15 @@
 package com.example.demo.student;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service
 public class StudentService {
+
     public List<Student> getStudent() {
         return List.of(
                 new Student(
@@ -16,4 +21,5 @@ public class StudentService {
                 )
         );
     }
+
 }
